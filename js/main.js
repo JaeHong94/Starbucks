5b4034d3ba512f7453f4812485f6516ee79c88ad
JaeHongMin94 <moonshin123@naver.com>
2021-05-28 16:23:08 +0900

    Implement header mark up and style

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
+const badgeEl = document.querySelector('header .badges');
 
 searchEl.addEventListener('click', function () {
   searchInputEl.focus();
@@ -14,3 +15,22 @@ searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 });
+
+window.addEventListener(
+  'scroll',
+  _.throttle(function () {
+    if (window.scrollY > 500) {
+      // Badge hide
+      gsap.to(badgeEl, 0.4, {
+        opacity: 0,
+        display: 'none',
+      });
+    } else {
+      // Badge visible
+      gsap.to(badgeEl, 0.4, {
+        opacity: 1,
+        display: 'block',
+      });
+    }
+  }, 300)
+);
